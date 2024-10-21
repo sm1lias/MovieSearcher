7b09e8b7de309e6a42913dbb99886c91d10990c7
s.miliaresis <smiliaressis@gmail.com>
2024-10-21 17:45:58 +0300

    completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,16 +61,9 @@ android {
 
     dependencies {
 
-        implementation("com.pierfrancescosoffritti.androidyoutubeplayer:core:12.1.0")
-
-
-        implementation("androidx.navigation:navigation-compose:2.6.0")
-
         implementation("com.squareup.retrofit2:retrofit:2.9.0")
         implementation("com.squareup.retrofit2:converter-gson:2.9.0")
         implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
-
-        implementation("androidx.paging:paging-compose:3.2.0")
         implementation("androidx.paging:paging-runtime-ktx:3.3.2")
 
         implementation("io.coil-kt:coil-compose:2.4.0")
@@ -80,46 +73,23 @@ android {
         implementation("com.google.android.material:material:1.9.0")
         kapt("com.google.dagger:hilt-android-compiler:2.48")
 
-        implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-
         implementation("androidx.datastore:datastore-preferences:1.0.0")
-
-        implementation("androidx.compose.material:material-icons-extended-android:1.6.0-alpha02")
-        implementation("androidx.compose.material:material:1.4.3")
-
-
-
         implementation("androidx.core:core-ktx:1.9.0")
         implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
         implementation("androidx.activity:activity-compose:1.7.0")
         implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-        implementation("androidx.compose.ui:ui")
-        implementation("androidx.compose.ui:ui-graphics")
-        implementation("androidx.compose.ui:ui-tooling-preview")
-        implementation("androidx.compose.material3:material3:1.2.0-alpha04")
         testImplementation("junit:junit:4.13.2")
         androidTestImplementation("androidx.test.ext:junit:1.1.5")
         androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-        androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-        androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-        debugImplementation("androidx.compose.ui:ui-tooling")
-        debugImplementation("androidx.compose.ui:ui-test-manifest")
-
-
         implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
-
         implementation("io.coil-kt.coil3:coil:3.0.0-rc01")
         implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.0-rc01")
-
-        //swipe refresh layout
         implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
         testImplementation ("app.cash.turbine:turbine:1.0.0")
-
-        testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.2") // For testing coroutines
-
-        testImplementation ("org.mockito:mockito-core:5.0.0") // Mockito for mocking
-        testImplementation ("org.mockito.kotlin:mockito-kotlin:5.0.0") // Kotlin extensions for Mockito
+        testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.2")
+        testImplementation ("org.mockito:mockito-core:5.0.0")
+        testImplementation ("org.mockito.kotlin:mockito-kotlin:5.0.0")
 
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "MovieRama"
+rootProject.name = "MovieSearcher"
 include(":app")
  
